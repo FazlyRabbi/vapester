9c9258da9c339ac16b8cae75650e394dbb83491f
BayajidAlam <bayajidalam2001@gmail.com>
2023-04-27 13:17:51 +0600

    products grid added

--- components/ShopPage/Layout/Layout.js
+++ components/ShopPage/Layout/Layout.js
@@ -1,12 +1,20 @@
 import LeftSideContainer from "./LeftSideContainer";
 import RightSideContainer from "./RightSideContainer";
+import styles from "/styles/Shop.module.css";
 
 function ShopLayout() {
   return (
-    <section className="grid gap-8 grid-cols-12 container mx-auto">
-      <LeftSideContainer/>
-      <RightSideContainer/>
-    </section>
+    <>
+      <section className="grid gap-8 grid-cols-12 container mx-auto">
+        <LeftSideContainer />
+        <RightSideContainer />
+      </section>
+      {/* workshop banner  */}
+
+      <div className={`${styles.discount1} container mx-auto h-96 bg-no-repeat relative min-h-[10rem] bg-center bg-cover grid place-items-center mt-48 mb-20`}>
+        <p>pskdjhsslds</p>
+      </div>
+    </>
   );
 }
 
--- components/ShopPage/Layout/RightSideContainer.js
+++ components/ShopPage/Layout/RightSideContainer.js
@@ -1,9 +1,13 @@
-import { FaList } from 'react-icons/fa';
+import Image from 'next/image';
+import { FaList, FaStar } from 'react-icons/fa';
 import { TbLayoutGrid } from 'react-icons/tb';
+import product1 from "/img/prod01-1-min-copyright-500x598.jpg";
 
 function RightSideContainer() {
   return (
     <section className="col-span-9">
+
+      {/* top section  */}
       <div className='flex justify-between items-center'>
         <div className='flex justify-between items-center gap-12'>
           <select className="px-12 py-4 text-white bg-[#393939]" name="cars" id="cars">
@@ -19,6 +23,237 @@ function RightSideContainer() {
             <p className='bg-[#D22756] text-white w-12 h-12 flex justify-center items-center'><TbLayoutGrid className='text-2xl'/></p>
             <p className='bg-[#393939] text-white w-12 h-12 flex justify-center items-center'><FaList className='text-2xl'/></p>
         </div>
+      </div>
+
+
+      {/*products in grid  */}
+      <div className='my-20 pt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-auto'>
+
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
+      <div className="text-left ">
+            <div className="border-4 border-[#EEEEEE] hover:border-[#393939] ">
+              <Image
+                height={300}
+                className={`max-h-[28rem] scale-90 hover:scale-100 overflow-hidden transition ease-in duration-300 p-1 w-full`}
+                alt="product__imgae"
+                src={product1}
+              />
+            </div>
+            <div className="my-4">
+              <p>DINNER LADY BLACKBERRY</p>
+              <div className="flex items-center">
+                <p className="text-pink-600">
+                  $<span className="text-[25px]">09.90</span>
+                </p>
+                <div className="text-center gap-1 flex justify-center text-yellow-300 ">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
+            </div>
+          </div>  
       </div>
     </section>
   );
