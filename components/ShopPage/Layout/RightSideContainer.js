0a1a30d30fb4be372180690a92ce41da22e27557
BayajidAlam <bayajidalam2001@gmail.com>
2023-04-27 12:30:09 +0600

    filter and change view section added

--- components/ShopPage/Layout/RightSideContainer.js
+++ components/ShopPage/Layout/RightSideContainer.js
@@ -1,5 +1,27 @@
-function RightSideContainer(){
-  return(<section className="col-span-9 bg-blue-800">sddsdsd</section>)
+import { FaList } from 'react-icons/fa';
+import { TbLayoutGrid } from 'react-icons/tb';
+
+function RightSideContainer() {
+  return (
+    <section className="col-span-9">
+      <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center gap-12'>
+          <select className="px-12 py-4 text-white bg-[#393939]" name="cars" id="cars">
+            <option value="volvo">sort by average rate</option>
+            <option value="volvo">sort by popularity</option>
+            <option value="volvo">sort by latest</option>
+            <option value="volvo">sort by price:low to high</option>
+            <option value="volvo">sort by price:high to low</option>
+          </select>
+          <p>show all 9 results</p>
+        </div>
+        <div className='flex justify-between items-center gap-2'>
+            <p className='bg-[#D22756] text-white w-12 h-12 flex justify-center items-center'><TbLayoutGrid className='text-2xl'/></p>
+            <p className='bg-[#393939] text-white w-12 h-12 flex justify-center items-center'><FaList className='text-2xl'/></p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default RightSideContainer;
