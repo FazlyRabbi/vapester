7122c4688e4e69a6a829801588b3f54f2b7a20f4
BayajidAlam <bayajidalam2001@gmail.com>
2023-04-27 21:45:54 +0600

    all at once

--- components/DynamicBanner.js
+++ components/DynamicBanner.js
@@ -7,7 +7,7 @@ function DynamicBanner({ children }) {
     >
       <div className="relative container mx-auto lg:pt-60 pt-12">
         <div className="text-center absolute lg:top-1/2 lg:left-0 left-4  text-white mx-0 bg-[#D22756] pr-8">
-          <p className="font-black uppercase lg:text-[80px] text-[40px] lg:-mt-6 -mt-4">{children}</p>
+          <p className="font-black uppercase lg:text-[80px] text-[30px] lg:-mt-6 -mt-3">{children}</p>
         </div>
       </div>
     </section>
--- components/ShopPage/Discount.jsx
+++ components/ShopPage/Discount.jsx
@@ -6,11 +6,11 @@ export default function DiscountBanner() {
       className={`${styles.discount1} container mx-auto h-96 bg-no-repeat relative min-h-[10rem] bg-center p-0 bg-cover grid place-items-center`}
     >
       <div className="relative w-full h-full">
-        <div className="text-center absolute right-[18%] top-[30%]  text-white mx-0 bg-[#AF7FFC] h-40">
-          <p className="font-black uppercase text-[80px] tracking-widest  -mt-7 text-center font-bun">
+        <div className="lg:text-center absolute lg:right-[18%] right-6 lg:top-[30%] top-[40%]  text-white lg:mx-0 bg-[#AF7FFC] lg:h-40 h-12 w-[90%] lg:w-full mx-auto">
+          <p className="font-black uppercase lg:text-[80px] text-[25px] tracking-widest  lg:-mt-7 -mt-2 text-center font-bun">
             {" "}
             SAVES 30% ON <br />
-            <span className="mt-0 uppercase tracking-widest font-black"> popular e liquids</span>
+            <span className="mt-0 uppercase tracking-widest font-black text-[25px]"> popular e liquids</span>
           </p>
         </div>
       </div>
