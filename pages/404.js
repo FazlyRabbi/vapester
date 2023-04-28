799870e4f0be4d8c89d812b1159dc3d0e1fb23c4
kingdomofkushking1070bc <kingdomofkushking1070bc@gmail.com>
2023-04-28 07:56:02 +0600

    fix issues

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,7 @@
-import Layout from "@/components/Layout";
 export default function notFound() {
   return (
     <div>
-      <Layout>
-        <h1>404</h1>
-      </Layout>
+      <h1>404</h1>
     </div>
   );
 }
